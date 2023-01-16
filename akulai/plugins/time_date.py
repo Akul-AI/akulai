d0ba7d2a714884c9f7bba0ecba957b317d70ef1d
Akul Goel <akulgoel2010@gmail.com>
2023-01-16 12:41:48 -0500

    Update time.py (now time_date.py)

--- akulai/plugins/time_date.py
+++ akulai/plugins/time_date.py
@@ -1,5 +1,7 @@
+# This file does anything relating to time/date/calendar... you get the idea.
 import time
 
 def handle(command):
     if "time" in command:
         print("The current time is:", time.strftime("%H:%M:%S"))
+    
