3720e1bee382c17fc8b919820040c8c386eb4e87
Akul Goel <akulgoel2010@gmail.com>
2023-01-18 18:58:32 -0500

    Make it so that JS plugins don't need to be stated
    JS Plugins don't need to be stated explicitly anymore, they are automatically detected too.

--- akulai/akulai.py
+++ akulai/akulai.py
@@ -24,16 +24,21 @@ class AkulAI:
 
     def discover_plugins(self):
         for file in os.listdir("plugins"):
-            if file.endswith(".py"):
+            if file.endswith(".py") or file.endswith(".js"):
                 plugin_name = os.path.splitext(file)[0]
-                self.plugins[plugin_name] = {"handle": self.load_plugin(file), "extension": ".py"}
+                extension = os.path.splitext(file)[1]
+                self.plugins[plugin_name] = {"handle": self.load_plugin(file, extension), "extension": extension}
 
-    def load_plugin(self, file):
-        module_name = os.path.splitext(file)[0]
-        spec = importlib.util.spec_from_file_location(module_name, f"plugins/{file}")
-        plugin = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(plugin)
-        return plugin.handle
+    def load_plugin(self, file, extension):
+        if extension == ".py":
+            module_name = os.path.splitext(file)[0]
+            spec = importlib.util.spec_from_file_location(module_name, f"plugins/{file}")
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            return plugin.handle
+        elif extension == ".js":
+            with open(f"plugins/{file}", "r") as f:
+                return f.read()
 
     def listen(self):
         while not self.stop_listening.is_set():
@@ -48,14 +53,13 @@ class AkulAI:
         for plugin_name in self.plugins:
             if plugin_name in command:
                 try:
-                    plugin_module = self.load_plugin(plugin_name)
+                    plugin_module = self.plugins[plugin_name]
                     if plugin_module["extension"]=='.py':
                         plugin_module["handle"](self,command)
                     elif plugin_module["extension"]=='.js':
-                        # bridge setup 
                         PyNodeJS.execute_js(f'''
                             const akulAI = {self};
-                            {plugin_module["code"]}
+                            {plugin_module["handle"]}
                         ''')
                 except Exception as e:
                     self.speak(f"An error occurred while running the plugin {plugin_name}: {str(e)}")
