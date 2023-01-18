c2bee0994947c33c3c39e45ffa876071659356d0
Akul Goel <akulgoel2010@gmail.com>
2023-01-18 17:54:10 -0500

    Create small_talk.py

---
+++ akulai/plugins/small_talk.py
@@ -0,0 +1,11 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot("AkulAI")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english.greetings",
+              "chatterbot.corpus.english.conversations")
+
+def handle(akulai, command):
+    response = chatbot.get_response(command)
+    akulai.speak(response)
