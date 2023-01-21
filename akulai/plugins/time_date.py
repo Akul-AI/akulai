11a7bd602418ee7397c997743a7f3d21aa5dac00
Akul Goel <akulgoel2010@gmail.com>
2023-01-21 16:27:56 -0500

    Add date to `time_date.py` file and chage print function to speak

--- akulai/plugins/time_date.py
+++ akulai/plugins/time_date.py
@@ -1,7 +1,13 @@
-# This file does anything relating to time/date/calendar... you get the idea.
-import time
+# This file does anything relating to time/date.
+import datetime
+
+# define all commonly used variables here
+now = datetime.datetime.now()
 
 def handle(command):
     if "time" in command:
-        print("The current time is:", time.strftime("%H:%M:%S"))
-    
+        time_now = now.strftime("%H:%M:%S")
+        akulai.speak(f"The current time is{time_now}")
+    if "date" in command:
+        date_now = now.strftime("%Y-%m-%d") 
+        akulai.speak(f"The current date is{date_now}")
