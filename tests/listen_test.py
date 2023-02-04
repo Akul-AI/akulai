94197629968e27e7b06cac22575e38d894d480d1
Akul Goel <akulgoel2010@gmail.com>
2023-02-04 11:38:33 -0500

    Create listen_test.py

---
+++ tests/listen_test.py
@@ -0,0 +1,7 @@
+from akulai.akulai import AkulAI
+
+akulai = AkulAI
+
+print("If this is working, whatever you say should print on the console.")
+print("Starting listening... now.")
+akulai.listen()
