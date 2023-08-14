b9385a3514db05bcd5a55fe50e617b1701db4dc1
Akul Goel <akulgoel2010@gmail.com>
2023-08-13 21:43:50 -0400

    Add plugin files

---
+++ plugins/time_date/main.py
@@ -0,0 +1,16 @@
+import requests
+import datetime
+
+# define all commonly used variables here
+now = datetime.datetime.now()
+
+response = requests.get('http://127.0.0.1:8000/speak')
+def handle(command):
+
+    if "time" in command:
+        time_now = now.strftime("%H:%M:%S")
+        speak(f"The current time is{time_now}")
+    
+    if "date" in command:
+        date_now = now.strftime("%Y-%m-%d") 
+        speak(f"The current date is{date_now}")
